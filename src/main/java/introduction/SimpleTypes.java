4396946d36fb40aa7ddb9728a8c0d2e9583e64dd
Marek Grota <marekgro@gmail.com>
2020-01-26 14:57:25 +0100

    Added more examples qith variables

--- src/main/java/introduction/SimpleTypes.java
+++ src/main/java/introduction/SimpleTypes.java
@@ -22,8 +22,20 @@ public class SimpleTypes {
         System.out.println(zmienna);
 
         boolean isPaid = false;
-        System.out.println("Czy zapłacono rachunek? " + isPaid);
+        String isPaidTxt = isPaid ? "tak" : "nie";
+        System.out.println("Czy zapłacono rachunek? " + isPaidTxt);
         isPaid = true;
-        System.out.println(" A może teraz już zapłacono? " + isPaid);
+        isPaidTxt = isPaid ? "tak" : "nie";
+        System.out.println("A może teraz już zapłacono? " + isPaidTxt);
+
+        int index = 0;
+        String name = "Andrzej";
+        char letter = name.charAt(index);
+        System.out.println((index + 1) + " literka w imieniu " + name + " to " + letter);
+
+        String prefix = "An";
+        boolean isStartingWith = name.startsWith(prefix);
+        System.out.println("Czy imię " + name + " zaczyna się od " + prefix + "?: " + isStartingWith);
+
     }
 }
