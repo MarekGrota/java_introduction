65e35bff298478443123561f7ad8f7877f7797a7
Marek Grota <marekgro@gmail.com>
2020-02-02 11:02:41 +0100

    Add basic examples for classes

---
+++ src/main/java/classes/basics/Test.java
@@ -0,0 +1,26 @@
+package classes.basics;
+
+public class Test {
+    public static void main(String[] args) {
+        String nazwisko = "Kowalski";
+        User adam = new User("Adam", nazwisko, 45);
+//        adam.name = "Adam";
+//        adam.lastname = "Kowalski";
+//        adam.age = 36;
+        System.out.println(adam.name);
+        System.out.println(adam.lastname);
+        System.out.println(adam.age);
+
+        User jozek = new User("Józek", "Nowak", 42);
+        jozek.introduce();
+
+        User cloneJozek = new User(jozek);
+        cloneJozek.introduce();
+        cloneJozek.name = "Robert";
+        System.out.println();
+        System.out.println("Po zmaianie imienia: ");
+        cloneJozek.introduce();
+
+        System.out.println("Józek: " + jozek + ", Robert: " + cloneJozek);
+    }
+}
