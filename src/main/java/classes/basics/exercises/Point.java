e5e4a2a37cfb96090f84fed1636a5a292bee0b52
Marek Grota <marekgro@gmail.com>
2020-02-04 20:42:46 +0100

    Fix some issue

---
+++ src/main/java/classes/basics/exercises/Point.java
@@ -0,0 +1,32 @@
+package classes.basics.exercises;
+
+public class Point {
+
+    private double x;
+    private double y;
+
+    public Point (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+    }
+//metody są rozpoznawane po nazwie i ilości parametrów
+    public double distanceTo(Point other) {
+        return distance(this, other);
+    }
+
+    public String toString() {
+        return  "Point(x: " + x + ", y: " + y + ")";
+    }
+
+    public double getX() {
+        return  x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
