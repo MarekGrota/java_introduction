f2a6f6cd74cbd29d0fd83f0aa7b001886334bdb9
Marek Grota <marekgro@gmail.com>
2020-02-07 21:23:37 +0100

    Code optimized (4 lines of code deleted)

--- src/main/java/homework/weekend01/Triangle.java
+++ src/main/java/homework/weekend01/Triangle.java
@@ -7,18 +7,14 @@ public class Triangle {
 
         Scanner scanner = new Scanner(System.in);
         int n = 0;
-        int i = 0;
-        char star = '*';
-        char empty = ' ';
         System.out.println(" Podaj N: ");
         n = scanner.nextInt();
 
-        for (i = 0; i < n; n--) {
+        for (int i = 0; i < n; n--) {
             for (int j = 0; j < n; j++) {
-                System.out.print(star);
+                System.out.print("*");
             }
             System.out.println();
-            while (n ==0);
         }
 
     }
